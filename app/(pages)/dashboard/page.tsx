ac23fa0b6b45643ae035abb588c16c1aba1e3521
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2025-04-28 21:02:24 +0300

    Commited

--- app/(pages)/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Page() {
-    return (
-        <>
-            <p>Home</p>
-        </>
-    )
-}
--- app/(pages)/dashboard/products/purchase-orders/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-        <>
-            <p>Here you can purhcase orders.</p>
-            <p>This creates an invoice with these purchases.</p>
-        </>
-    )
-}
